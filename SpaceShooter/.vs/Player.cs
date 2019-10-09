a17df9d80fbb2500099ddfa260fece58837abd49
MarioBarsum <43605179+MarioBarsum@users.noreply.github.com>
2019-10-09 13:40:49 +0200

    test

---
+++ SpaceShooter/.vs/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace SpaceShooter {
+
+    class Player
+    {
+            Texture2D gfx;
+            Vector2 position;
+            Vector2 speed;
+
+            public Player(Texture2D texture, float X, float Y, float speedX, float speedY)
+            {
+                this.gfx = texture;
+                this.position.X = X;
+                this.position.Y = Y;
+            }
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
---
+++ SpaceShooter/Player.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SpaceShooter
+{
+    class Player
+    {
+        Texture2D texture;
+        Vector2 position;
+        Vector2 speed;
+
+        public Player(Texture2D texture, float X, float Y, float speedX, float speedY)
+        {
+            this.texture = texture;
+            this.position.X = X;
+            this.position.Y = Y;
+            this.speed.X = speedX;
+            this.speed.Y = speedY;
+        }
+
+        public void Update(GameWindow window)
+        {
+            KeyboardState keyboardState = Keyboard.GetState();
+        
+           
+            if(keyboardState.IsKeyDown(Keys.Right) || keyboardState.IsKeyDown(Keys.D))
+            {
+                position.X += speed.X;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Left) || keyboardState.IsKeyDown(Keys.A))
+            {
+                position.X -= position.X;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Up) || keyboardState.IsKeyDown(Keys.W))
+            {
+                position.Y -= speed.Y;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Down) || keyboardState.IsKeyDown(Keys.S))
+            
+            {
+                position.Y += speed.Y;
+            }
+        }
+    }   
+}
